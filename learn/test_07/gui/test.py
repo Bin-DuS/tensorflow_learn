d1ef5fbdcb956b40908c1659c1516121300f09b9
duzhengbin <847479962@qq.com>
2018-09-22 12:22:04 +0800

    python实现GUI,增加label和button

--- learn/test_07/gui/test.py
+++ learn/test_07/gui/test.py
@@ -1,3 +1,9 @@
 import tkinter as tk
 root = tk.Tk()
+label = tk.Label(root,text = "hello world!")
+label.pack()
+btn = tk.Button(root,text = "OK")
+btn.pack();
+entry = tk.Entry(root)
+entry.pack()
 root.mainloop()
